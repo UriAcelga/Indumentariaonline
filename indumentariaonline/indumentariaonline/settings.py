a9647e1e98ac20266d1bee94ac60a91077cec433
Uriel Gomez <urigomez2002@gmail.com>
2022-11-26 11:26:24 -0300

    Add files via upload

--- indumentariaonline/indumentariaonline/settings.py
+++ indumentariaonline/indumentariaonline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^2+&%c1_t&&hywkndd1*!$hp8f$^n-mx-b-g5=7zn%g=g4aea&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
